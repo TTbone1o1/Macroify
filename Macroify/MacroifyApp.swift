84659e3fa94846b66e2223a56cc3750e2f0a6875
Abraham May <Abrahammay1015@gmail.com>
2024-10-08 16:26:02 -0500

    Initial Commit

---
+++ Macroify/MacroifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroifyApp.swift
+//  Macroify
+//
+//  Created by Abraham May on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MacroifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacroifyTests/MacroifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacroifyTests.swift
+//  MacroifyTests
+//
+//  Created by Abraham May on 10/8/24.
+//
+
+import Testing
+@testable import Macroify
+
+struct MacroifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
